5c01ab1de44c648f4f34c4ad5735dbb2f9fc7dcb
Nuree Kim <kimnuree2021@gmail.com>
2026-09-07 19:00:38 +0200

    sturcture study2

--- 20260907_1.cpp
+++ 20260907_1.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-/* int main()
+int main()
 {
     struct Oppa
     {
@@ -37,27 +37,28 @@ using namespace std;
 
     return 0;
 }
- */
+
 //공용체union 와 열거체 enum class)
 
-main()
+int main()
 {
-    union Data
-    {
-        int i;
-        float f;
-        char str[20];
-    };
+    //열거체 기호 상수를 만드는 것에 대한 또다른 방법
+    enum spectrum {red, orange, yellow, green, blue, indigo, violet};
+    /* 
+    1. spectrum은 열거체의 이름입니다.
+    2. red, orange, yellow, green, blue, indigo, violet은 열거체의 기호 상수입니다.'0,에서부터 7까지 각각 대응하게됨
+     0,1,2,3,4,5,6 이렇게됨
+    
+    */
+   spectrum color = red;
+   cout << "color is " << color << endl;
+   
+   return 0;
 
-    Data data;
-    data.i = 10;
-    cout << "data.i: " << data.i << endl;
+   enum spectrum {red=1, orange=3, yellow, green, blue, indigo, violet};
+  // 이것과 동일함 > enum spectrum {red=1, orange=3, yellow=4, green=5, blue=6, indigo=7, violet=8}; 초기화 하지 않은 열거자들은 그이전 값보다 1씩 증가해서 자동으로 지정됨
 
-    data.f = 220.5;
-    cout << "data.f: " << data.f << endl;
+  
 
-    strcpy(data.str, "Hello");
-    cout << "data.str: " << data.str << endl;
-
-    return 0;
 }
+
