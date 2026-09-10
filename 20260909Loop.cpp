1de61484f740ea29c0da8922b09d1383851f05d7
Nuree Kim <kimnuree2021@gmail.com>
2026-09-10 11:02:39 +0200

    new study

---
+++ 20260909Loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    for (int i = 0; i <  5; i++)
+    { cout << i << "th"<< endl;
+
+        /*  */
+
+    }
+return 0;
+int a = 10;
+int b = 10;
+
+cout << a << b << endl;
+cout << a++ << ++b << endl;//10, 11
+cout << a << b << endl; // 11,11
+// ++z 이 앞에 있으면 증가시키고 출력 뒤에 있으면 출력하고 증가 
+}
